6c3191c3fc31f501c12af9842a8c5ba2a9467e52
rabi212 <144771474+rabi212@users.noreply.github.com>
2024-07-29 13:52:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "google" {
-  project = "gcp-poc-project-285306" # Replace with your actual project ID
-  region  = "us-central1"
+  project = "gcp-poc-project" # Replace with your actual project ID
+  region  = "asia-south1"
 }
 
 resource "google_storage_bucket" "my_bucket" {
   name                     = "compute96-unique" # Ensure this name is globally unique
-  location                 = "US"
+  location                 = "asia-south1"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
